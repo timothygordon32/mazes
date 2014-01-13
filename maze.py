a69619e32a24f561805f4d9a73a48ccf8ebe1178
Roger Bush <r.bush@virgin.net>
2014-01-13 23:00:58 +0000

    Refuctored it a bit. You will need a colour TV licence for this now.

--- maze.py
+++ maze.py
@@ -1,12 +1,37 @@
 import numpy
 from numpy.random import random_integers as rand
 import matplotlib.pyplot as pyplot
-import matplotlib.colors as colors
 from matplotlib import animation
 
-maze_start = 2
-maze_finish = 3
-maze_player = 4
+palettes = {
+    # RGB vectors in a 0-1 format palatable to matplotlib
+    # Once a palette is selected (e.g. 'maze') the boolean values in the maze are mapped to the appropriate color
+    'maze':             numpy.array( [ [1.0, 1.0, 1.0],     # index 0 = paths = white
+                                       [0.0, 0.0, 0.0] ] ), # index 1 = walls = black
+
+    'known_to_player':  numpy.array( [ [0.0, 0.0, 0.0],     # index 0 = unknown territory = black
+                                       [0.0, 1.0, 0.5] ] ), # index 1 = known paths = green
+
+    'known_to_minator': numpy.array( [ [0.0, 0.0, 0.0],     # index 0 = unknown territory = black
+                                       [1.0, 0.5, 0.0] ] ), # index 1 = known paths = orange
+
+    'player_location':  numpy.array( [ [0.0, 0.0, 0.0],     # index 0 = not here = black - check this works when averaging colours
+                                       [0.0, 0.0, 1.0] ] ), # index 1 = here = blue
+
+    'minator_location': numpy.array( [ [0.0, 0.0, 0.0],     # index 0 = not here = black - check this works when averaging colours
+                                       [1.0, 0.0, 0.0] ] )  # index 1 = here = red
+}
+
+#print palettes['maze']
+#print palettes['known_to_player'][1]
+
+# The idea is that we keep the maze (and maze-like things such as known territory) boolean and separate from each other
+# and manage the rendering later via function CreateImage etc.
+
+def CreateImage(boolean_array, palette_name):
+    # The next statement uses an array as an index to another array.  Gotta love Python!
+    # http://wiki.scipy.org/Tentative_NumPy_Tutorial#head-3f4d28139e045a442f78c5218c379af64c2c8c9e
+    return palettes[palette_name][boolean_array.astype(int)]
 
 def generate(width=81, height=51, complexity=.75, density=.75):
     # Only odd shapes
@@ -17,7 +42,7 @@ def generate(width=81, height=51, complexity=.75, density=.75):
     # Build actual maze
     Z = numpy.zeros(shape, dtype=bool)
     # Fill borders
-    Z[0, :] = Z[-1, :] = 1
+    Z[0, :] = Z[-1, :] = 1  # 1 (True) is a wall, 0 (False) is a path
     Z[:, 0] = Z[:, -1] = 1
     # Make isles
     for i in range(density):
@@ -37,38 +62,27 @@ def generate(width=81, height=51, complexity=.75, density=.75):
                     x, y = x_, y_
     return Z
 
-def show(maze):
-
-    render = maze.astype(numpy.int)
-
-    render[1, 1] = maze_start
-    render[-2, -2] = maze_finish
-
+def show_maze(maze):
+    image = CreateImage(boolean_array=maze, palette_name='maze')
     pyplot.figure(figsize=(10, 5))
     pyplot.xticks([]), pyplot.yticks([])
-    pyplot.imshow(render, cmap=colors.ListedColormap(['black', 'white', 'red', 'green', 'yellow'], 'indexed'), interpolation='nearest')
+    pyplot.imshow(image, interpolation='nearest')
     pyplot.show()
 
-def show2(maze):
-
-    render = maze.astype(numpy.int)
-
-    render[1, 1] = maze_start
-    render[-2, -2] = maze_finish
-
+def animate_maze(maze):
+    image = CreateImage(boolean_array=maze, palette_name='maze')
     fig = pyplot.figure(figsize=(10, 5))
     pyplot.xticks([]), pyplot.yticks([])
-    im = pyplot.imshow(render, cmap=colors.ListedColormap(['black', 'white', 'red', 'green', 'yellow'], 'indexed'), interpolation='nearest')
+    imgplot = pyplot.imshow(image, interpolation='nearest')
 
-    def animate(i):
-        render[1, 1 + i] = maze_player
-        im.set_data(render)
-        return im
+    def CreateFrame(frame):
+        image[1, 1 + frame] = palettes['known_to_player'][1]
+        imgplot.set_data(image)
+        return imgplot
 
-    anim = animation.FuncAnimation(fig, animate, frames=79, interval=200)
-
-    # anim.save('basic_animation.mp4', fps=1)
-
+    anim = animation.FuncAnimation(fig, CreateFrame, frames=maze.shape[1]-2, interval=50)
+    #anim.save('myAnimatedMaze.mp4')
     pyplot.show()
 
-show2(maze = generate(80, 40, 0.9, 0.9))
+#show_maze(maze = generate(80, 40, 0.9, 0.9))
+animate_maze(maze = generate(80, 40, 0.9, 0.9))
